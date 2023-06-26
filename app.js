cff7142d750a734683d7962bbf69e3341a1888ee
anhson <sonanhnguyen003@gmai.com>
2023-06-26 23:23:40 +0700

    update UI

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.use (express.static(__dirname + '/public'));
 app.get("/", function(req,res){
     res.render('index', {title: "ToDo App"});
 })
+const PORT = process.env.PORT || 5001;
 
-app.listen(5001,function(){
-    console.log("Listening on port 5001!");
-});
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+  });
