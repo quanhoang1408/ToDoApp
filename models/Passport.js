51f22d41f6208158d9c79cb7a35d0e8418850862
Nguyễn Anh Sơn <91643074+nas03@users.noreply.github.com>
2023-07-01 13:40:58 +0700

    update node modules

--- models/Passport.js
+++ models/Passport.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 
 function initialize(passport, getUserByEmail, getUserById) {
